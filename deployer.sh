50215432c930b28509aae48e277f2e5d125b6fe1
Lin Diniz <lin@fnxln.com>
2022-04-24 15:46:02 -0300

    Changed stuff

--- deployer.sh
+++ deployer.sh
@@ -15,18 +15,8 @@ jpegoptim --size=10k ./static/*.jpeg
 jpegoptim --size=10k ./static/*/*.jpg
 jpegoptim --size=10k ./static/*/*.jeg
 
-# gera o html 
-hugo --gc --noTimes &&
-
-# commita os assets publicos 
-cd public/ &&
+# commita os assets publicos
 git add . &&
 git commit -m "new post ${DATA}" &&
 git push origin main &&
 
-# commita o html completo 
-cd .. &&
-git add . &&
-git commit -m "new post ${DATA}" &&
-git push origin main &&
-clear
